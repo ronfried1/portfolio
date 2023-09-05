801afa28a8c94a0e1dc73dbf83a15cef1311fe6b
ron <ron.fried15@gmail.com>
2023-09-06 02:02:24 +0300

    fix

--- src/app/components/hero/CardBody.tsx
+++ src/app/components/hero/CardBody.tsx
@@ -8,10 +8,9 @@ const CardBody = (props: Props) => {
       className={" animate-slide-in-left mt-12 delay-700 text-white text-lg"}
     >
       <p className="xl:text-4xl">
-        {`
-        I'm a <strong>Full stack developer </strong>from
-        ${(<br />)}
-        Tel Aviv, Israel.`}
+        {"I'm"} a <strong>Full stack developer </strong>from
+        <br />
+        Tel Aviv, Israel.
       </p>
     </div>
   );
